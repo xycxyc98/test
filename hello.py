6eb4b247973035bccad610e33e95043eba680e3c
DESKTOP-LSU0ORG\YD <1415483840@qq.com>
2021-04-08 15:35:45 +0800

    no message

---
+++ hello.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+Author: XiaoYongChun
+Date: 2021-04-08 15:35:10
+LastEditTime: 2021-04-08 15:35:20
+LastEditors: XiaoYongChun
+Descripttion: 
+'''
+
+
+print("hello world")
