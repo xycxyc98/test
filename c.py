7a90b42d4cf595b0e2f46e210385690314b8670b
1904xyc <1415483840@qq.com>
2021-09-06 15:18:35 +0800

    四个c

--- c.py
+++ c.py
@@ -2,10 +2,10 @@
 '''
 Author: XiaoYongChun
 Date: 2021-09-06 15:11:19
-LastEditTime: 2021-09-06 15:17:05
+LastEditTime: 2021-09-06 15:18:27
 LastEditors: XiaoYongChun
 Descripttion: 
 '''
 
-print("cc")
+print("cccc")
 print("c")
